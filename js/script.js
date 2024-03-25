b9fb49be8ec705f033af937346c4c0d5f5a2c327
NikitaNevsky <40342871+NikitaNevsky@users.noreply.github.com>
2024-03-25 23:23:59 +0300

    Create new project

---
+++ js/script.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+ 
+    $("a.scroll-to").click(function() {
+       $("html, body").animate({
+          scrollTop: $($(this).attr("href")).offset().top + "px"
+       }, {
+          duration: 500,
+          easing: "swing"
+       });
+       return false;
+    });
+
+    $(".icon-menu").click(function() {
+      $(".menu__list").addClass('show');
+      $(".icon-menu").addClass('d-none');
+      $(".icon-close").addClass('show');
+    });
+
+    $(".icon-close").click(function() {
+      $(".menu__list").removeClass('show');
+      $(".icon-menu").removeClass('d-none');
+      $(".icon-close").removeClass('show');
+    });
+
+    $(".menu__close").click(function() {
+      $(".menu__list").removeClass('show');
+      $(".icon-menu").removeClass('d-none');
+      $(".icon-close").removeClass('show');
+    });
+
+    $('.services__slider').owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: true,
+      dots: true,
+      responsive:{
+          0:{
+              nav: true,
+              items:1
+          },
+          600:{
+              nav: true,
+              items:3
+          }
+      }
+    });
+
+    $('.input-file input[type=file]').on('change', function(){
+      let file = this.files[0];
+      $(this).next().html(file.name);
+    });
+  
+ });
